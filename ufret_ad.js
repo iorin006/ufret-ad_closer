2231620406952e6c7473c527410ac300e7967453
iorin006 <108205818+iorin006@users.noreply.github.com>
2024-09-04 09:34:32 +0900

    Update ufret_ad.js

--- ufret_ad.js
+++ ufret_ad.js
@@ -1,16 +1,14 @@
 // ==UserScript==
-// @name         ufret-adclose
-// @namespace    https://www.ufret.jp/
-// @version      2126-08-27
-// @description  ublock 必須
-// @author       iorin006
-// @match        https://www.ufret.jp/*
-// @icon         https://www.google.com/s2/favicons?sz=64&domain=ufret.jp
+// @name         Remove Adblock Thing
+// @namespace    http://tampermonkey.net/
+// @version      5.6
+// @description  Removes Adblock Thing
+// @author       JoelMatic
+// @match        https://www.youtube.com/*
+// @icon         https://www.google.com/s2/favicons?sz=64&domain=youtube.com
+// @updateURL    https://github.com/TheRealJoelmatic/RemoveAdblockThing/raw/main/Youtube-Ad-blocker-Reminder-Remover.user.js
+// @downloadURL  https://github.com/TheRealJoelmatic/RemoveAdblockThing/raw/main/Youtube-Ad-blocker-Reminder-Remover.user.js
 // @grant        none
-// @updateURL    https://raw.githubusercontent.com/iorin006/ufret-ad_closer/main/ufret_ad.js
-// @downloadURL  https://raw.githubusercontent.com/iorin006/ufret-ad_closer/main/ufret_ad.js
-// @supportURL   https://github.com/iorin006/ufret-ad_closer/issues
-// @homepage     https://github.com/iorin006/ufret-ad_closer
 // ==/UserScript==
 
 function remove_class(elements) {
