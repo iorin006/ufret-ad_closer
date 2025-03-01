8804a6b4228ac7e4d848127448919331b0996589
iorin006 <108205818+iorin006@users.noreply.github.com>
2025-03-01 20:39:04 +0900

    Update ufret-ad_close.user.js

--- ufret-ad_close.user.js
+++ ufret-ad_close.user.js
@@ -2,7 +2,7 @@
 // @name         ufret-ad_close
 // @namespace    http://tampermonkey.net/
 // @description  ublock推奨
-// @author       me
+// @author       me & ChatGPT
 // @match        https://www.ufret.jp/*
 // @icon         https://www.ufret.jp/favicon.ico
 // @updateURL    https://github.com/iorin006/ufret-ad_closer/raw/main/ufret-ad_close.js
@@ -10,49 +10,28 @@
 // @grant        none
 // ==/UserScript==
 
-function myFunction(e) {
-    e.preventDefault();
-    var now = new Date().getTime() / 10;
-    $.get('/web_api/set_ad_free_start_time.php', {
-        start_time: now
-    }, function (response) {
-       // window.location.reload();
-    }).fail(function () {
-        alert('広告非表示の設定に失敗しました。');
-    });
-}
+(() => {
+  $.get('/web_api/set_ad_free_start_time.php', { start_time: Date.now() / 10 })
+    .fail(() => alert('広告非表示の設定に失敗しました。'));
 
-myFunction(new Event('click'));
+  const removeByClass = cls =>
+    document.querySelectorAll('.' + cls.split(' ').join('.')).forEach(el => el.remove());
 
-function remove_class(elements) {
-    var jama = document.getElementsByClassName(elements);
-    if (jama.length > 0) {
-        for (var i = 0; i < jama.length; i++) {
-            jama[i].remove()
-        }
-    }
-}
+  setInterval(() => {
+    const adCloseBtn = document.getElementById('ufret-ad-close');
+    adCloseBtn && adCloseBtn.click();
 
-setInterval(() => {
+    document.querySelectorAll('img').forEach(img => img.setAttribute('width', '0%'));
 
-    var jama1 = document.getElementById('ufret-ad-close')
-    var images = document.getElementsByTagName('img')
-
-    if (jama1) {
-        jama1.click()
-    }
-
-    for (var i = 0; i < images.length; i++) {
-        images[i].setAttribute('width', '0%');
-    }
-
-    remove_class('notice-close-button');
-    remove_class('d-block w-100');
-    remove_class('btn btn-lg btn-danger btn-block');
-    remove_class('fixed-trial-banner__content d-flex');
-    remove_class('fixed-trial-banner container');
-    remove_class('fixed-premium-banner__content d-flex');
-    remove_class('fixed-premium-banner container');
-    remove_class('notice-background')
-
-}, 100);
+    [
+      'notice-close-button',
+      'd-block w-100',
+      'btn btn-lg btn-danger btn-block',
+      'fixed-trial-banner__content d-flex',
+      'fixed-trial-banner container',
+      'fixed-premium-banner__content d-flex',
+      'fixed-premium-banner container',
+      'notice-background'
+    ].forEach(removeByClass);
+  }, 100);
+})();
